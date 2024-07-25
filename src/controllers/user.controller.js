28f3938abfcf1fec9dc1755847b558383550e200
Kamal Garg <2311kamalgarg@gmail.com>
2024-07-26 02:20:28 +0530

    added new user routes for which controller has already been written

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -296,6 +296,72 @@ const updateUsercoverImage = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, user, "Cover Image updated successfully"));
 });
 
+const getUserChannerProfile = asyncHandler(async (req, ues) => {
+  const { username } = req.params;
+  if (!username.trim()) {
+    throw new apiError(400, "UserName is missing");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        userName: username?.toLowerCase(),
+      },
+    },
+    {
+      from: "subscriptions",
+      localField: "-id",
+      foreignField: "channel",
+      as: "subscribers",
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscriberToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.use?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: fasle,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscriberToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
+
 const getWathHistory = asyncHandler(async (req, res) => {
   const user = await User.aggregate([
     {
@@ -354,5 +420,6 @@ export {
   updateAccountDetail,
   updateUserAvatar,
   updateUsercoverImage,
+  getUserChannerProfile,
   getWathHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
-  loginUser,
   registerUser,
+  loginUser,
   logoutUser,
   refrehAccessToken,
+  changePassword,
+  getCurrentUser,
+  updateAccountDetail,
+  updateUserAvatar,
+  updateUsercoverImage,
+  getWathHistory,
+  getUserChannerProfile,
 } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
@@ -29,5 +36,11 @@ userRouter.route("/login").post(loginUser);
 //Secured Routes
 userRouter.route("/logout").post(verifyJWT, logoutUser);
 userRouter.route("/refresh-token").post(refrehAccessToken);
-
+userRouter.route("/change-pass").post(verifyJWT, changePassword);
+userRouter.route("/cur-user").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-details").patch(verifyJWT, updateAccountDetail);
+userRouter.route("/update-avatar").get(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter.route("/update-cover").get(verifyJWT, upload.single("coverImage"), updateUsercoverImage);
+userRouter.route("/channel/:username").get(verifyJWT,getUserChannerProfile)
+userRouter.route("/history").get(verifyJWT,getWathHistory)
 export default userRouter;
