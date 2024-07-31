92be9ebfda0323da8b379b5391133dcea56816f1
Kamal Garg <2311kamalgarg@gmail.com>
2024-07-31 15:07:32 +0530

    subscribing a user has been tested successfully

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,41 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {apiError} from "../utils/apiError.js"
-import {apiResponse} from "../utils/apiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const { channelId } = req.params;
+  const user = await User.findById(channelId);
+  if (!user) {
+    throw new apiError(404, "channel doesn't exist");
+  }
+
+  try {
+    const channel = await User.aggregate([
+      {
+        $match: {
+          _id: channelId,
+        },
+      },
+      {
+        $lookup: {
+          from: "videos",
+          localField: "_id",
+          foreignField: "owner",
+          as: "userVideos",
+        },
+      },
+    ]);
+  } catch (error) {}
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  // TODO: Get all the videos uploaded by the channel
+});
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+export { getChannelStats, getChannelVideos };
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,28 +1,65 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import {apiError} from "../utils/apiError.js"
-import {apiResponse} from "../utils/apiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+  const { channelId } = req.params;
+  // TODO: toggle subscription
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new apiError(403, "channer does not exist");
+  }
+  const userId = req.user?.id;
+  // console.log(userId);
+  // console.log(channelId, "\n", userId);
+  if (userId === channelId) {
+    throw new apiError(402, "Can not subscripbe to yourself");
+  }
+  const isSubscribed = await Subscription.findOne({
+    subscriber: userId,
+    channel: channelId,
+  });
+  let response;
+  try {
+    response = isSubscribed
+      ? await Subscription.deleteOne({ subscriber: userId, channel: channelId })
+      : await Subscription.create({ subscriber: userId, channel: channelId });
+  } catch (error) {
+    console.log("toggleSubscriptionError :: ", error);
+    throw new apiError(500, error?.message || "Internal server Error");
+  }
+  // console.log(channel.userName);
+  return res.status(200).json(
+    new apiResponse(
+      200,
+      {
+        response,
+        subscriber: req.user.userName,
+        subscribedTo: channel.userName,
+      },
+      isSubscribed === null
+        ? "subscribed Succesfully"
+        : "unsubscribed Sucessfully"
+    )
+  );
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+  const { channelId } = req.params;
+  const subscribers = Subscription.aggregate([
+    {
+      $match: channelId,
+    },
+  ]);
+});
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+  const { userId } = req.params;
+});
 
-export {
-    toggleSubscription,
-    getUserChannelSubscribers,
-    getSubscribedChannels
-}
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
