f92d2a78fc0d0b6fbd7f4d8b9491a6e9883c9898
Kamal Garg <2311kamalgarg@gmail.com>
2024-12-03 17:10:56 +0530

    changed the image size in userCard.jsx

--- frontend/src/components/user/userCard.jsx
+++ frontend/src/components/user/userCard.jsx
@@ -3,20 +3,18 @@ import { useSelector } from "react-redux";
 function UserCard({ className }) {
   const user = useSelector((state) => state.auth.user);
 
-  const subCount = "10M"; 
+  const subCount = "10M";
   return (
     <div className={`flex ${className}`}>
       <div className="flex overflow-hidden">
         <img
-          className="rounded-full"
+          className="rounded-full aspect-square w-40"
           src={user?.avatar || "https://via.placeholder.com/150"}
           alt="User Avatar"
         />
       </div>
       <div className="flex flex-col ml-4">
-        <div className="text-3xl">
-          {user?.fullName || "Full Name"}{" "}
-        </div>
+        <div className="text-3xl">{user?.fullName || "Full Name"} </div>
         <div className="flex">
           <div>@{user?.userName || "userName"}</div>{" "}
           <div className="ml-2">{subCount}</div>
